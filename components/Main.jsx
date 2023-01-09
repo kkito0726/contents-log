4ce5966de3472e241a14b97e6e2f067ac66bb5e7
kkito0726 <k.kito0726@gmail.com>
2023-01-10 00:07:52 +0900

    UI部分の実装

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,10 +4,13 @@ const url = "http://localhost:4000";
 
 export const Main = () => {
   const [data, setData] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [editId, setEditId] = useState([]);
+
   const getDb = async () => {
-      const res = await (await fetch(`${url}/booklog`)).json();
+    const res = await (await fetch(`${url}/booklog`)).json();
     console.log(res.status);
-      setData((prevData) => [...prevData, ...res]);
+    setData((prevData) => [...prevData, ...res]);
   };
 
   const deleteDb = async (id) => {
@@ -24,20 +27,65 @@ export const Main = () => {
     getDb();
   }, []);
 
+  const handleEdit = (id) => {
+    if (editId.indexOf(id) !== -1 && isEdit) return;
+    setIsEdit((prevIsEdit) => true);
+    setEditId((prevEditId) => [...prevEditId, id]);
+    console.log(editId);
+  };
+
+  const handleCancel = (id) => {
+    setEditId((prevEditId) => prevEditId.splice(prevEditId.indexOf(id), 1));
+    if (editId.length == 0) {
+      setIsEdit((prevIsEdit) => false);
+    }
+    console.log(editId);
+  };
+
   return (
     <div className="mainPage">
       <h1>消費コンテンツメモアプリ</h1>
 
-      <div>
+      <div className="contents">
         <ol>
           {data.map((jsonData) => {
             return (
-              <li
-                key={jsonData.id}
-              >{`Title: ${jsonData.title} Comment: ${jsonData.comment}`}</li>
+              <div key={jsonData.id} className="content">
+                <li>{`Title: ${jsonData.title} Comment: ${jsonData.comment}`}</li>
+                <button
+                  onClick={() => deleteDb(jsonData.id)}
+                  className="deleteButton"
+                >
+                  削除
+                </button>
+
+                {isEdit && editId.indexOf(jsonData.id) !== -1 ? (
+                  <>
+                    <input type="text" placeholder={jsonData.title} />
+                    <input type="text" placeholder={jsonData.comment} />
+                    <button>送信</button>
+                    <button onClick={() => handleCancel(jsonData.id)}>
+                      キャンセル
+                    </button>
+                  </>
+                ) : (
+                  <button onClick={() => handleEdit(jsonData.id)}>編集</button>
+                )}
+              </div>
             );
           })}
         </ol>
+      </div>
+
+      <div>
+        <label htmlFor="">
+          TITLE:
+          <input type="text" placeholder="Titleを入力" />
+        </label>
+        <label htmlFor="">
+          COMMENT:
+          <input type="text" placeholder="Commentを入力" />
+        </label>
       </div>
     </div>
   );
